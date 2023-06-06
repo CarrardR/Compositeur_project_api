381ad5dcefdbefbac0e161e0c70ee44cfbcd8d9d
CarrardR <remicarrard@bluewin.ch>
2023-06-06 15:39:05 +0200

    Update compoSpecifique.js

--- code/js/compoSpecifique.js
+++ code/js/compoSpecifique.js
@@ -11,7 +11,11 @@ class CompoSpecifiqueCtrl {
 
     afficherComposer(compositeur){
       http.chercherCompoSpecifique(compositeur, (data) => {
-        let s = "<div>" + data.composer.birth + "</div><div>" + data.composer.death + "</div><div>" + data.composer.epoch + "</div>"
+        let s = "<div>" + data.composer.birth + "</div><div>" + data.composer.death + "</div><div>" + data.composer.epoch + "</div><div class='liste'>"
+        for (const morceau in data.works) {
+          s += "<p>" + data.works[morceau].title + "</p>";
+        }
+        s += "</div>";
         $("#resultat").append(s);
         $("#image").append("<img src='" + data.composer.portrait + "'>");
         $("#titre").append(data.composer.complete_name);

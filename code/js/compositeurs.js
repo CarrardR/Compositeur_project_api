12d536392934685b1ecf085f47225ba59747b275
CarrardR <remicarrard@bluewin.ch>
2023-06-13 09:50:32 +0200

    Update compositeurs.js

--- code/js/compositeurs.js
+++ code/js/compositeurs.js
@@ -23,7 +23,7 @@ class CompositeursCtrl {
         $("#compositeurs").append(s);
       }
       $(".flip-card").hover(function () {
-        console.log($(this).find(".flip-card-inner .flip-card-front .nomComp p").val());
+        console.log($(this).find(".flip-card-inner").val());
       }
     );
     });
